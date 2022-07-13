bf441f8c27584c2c5481eb10b0946294b78a19ff
nikabramishvili <n.abramishvili@area.ge>
2022-07-13 20:54:46 +0400

    prog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CustomThemeService } from './custom-theme.service';
 
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-custom-theme';
+  title = 'my-app';
+  isDark = this.styleManager.isDark;
+
+  constructor(private styleManager: CustomThemeService) {}
+
+  toggleDarkTheme() {
+    this.styleManager.toggleDarkTheme();
+    this.isDark = !this.isDark;
+  }
 }
